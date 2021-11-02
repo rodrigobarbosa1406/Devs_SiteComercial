4bd9b35f29f2646115441752f3c39c62ea1259f2
rodrigobarbosa1406 <84208248+rodrigobarbosa1406@users.noreply.github.com>
2021-11-02 16:21:25 -0300

    20211102 - Template padrão para site comercial

---
+++ js/main.js
@@ -0,0 +1,17 @@
+(function($){
+    "use strict";
+    /* Inicializar o contador */
+    $('.contar').counterUp({
+        time: 1000,
+        delay: 10
+    });
+
+    var $doc = $('html,body');
+    $('.scroll-page').click(function(){
+        $doc.animate({
+            scrollTop: $($.attr(this,'href')).offset().top
+        }, 500);
+        return false;
+    });
+
+})(jQuery);
